9800c302a8f44d9153f326eb70f1c426043c1c8a
MrWolts <max.wolts@gmail.com>
2020-08-29 20:22:30 -0300

    end first part

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,5 +1,6 @@
 // logica de la resolucion de rutas.
 const resolveRoutes = (route) => {
+    //si tura tiene menos de 3 elementos significa que es el home o el id
     if (route.length <= 3) {
         let validRoute = route === '/' ? route : '/:id'
         return validRoute
